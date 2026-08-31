8c00bfd77f505a508c7889a8ba1e6435101331fb
Bhupendra Singh <bs4004908@gmail.com>
2026-08-31 14:18:20 +0530

    accounts api

--- src/models/account.model.js
+++ src/models/account.model.js
@@ -13,8 +13,8 @@ const accountSchema = new mongoose.Schema({
         enum: {
             values: ['ACTIVE', 'FROZEN', 'CLOSED'],
             message: 'Status must be either ACTIVE, FROZEN, or CLOSED',
-            default: 'ACTIVE'
-        }
+        },
+        default: 'ACTIVE'
     },
     currency:{
         type: String,
