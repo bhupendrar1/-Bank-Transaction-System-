80e7c8032bd6643b80ce306da9300ca1eec938f8
Bhupendra Singh <bs4004908@gmail.com>
2026-09-03 16:53:07 +0530

    accounts

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -1,6 +1,7 @@
-
-
-
+const transactinModel = require('../models/transaction.model');
+const ledgerModel = require('../models/ledger.model');
+const accountModel = require('../models/account.model');
+const emailService = require('../services/email.service');
 
 /**
  * - Create a new transaction
@@ -20,7 +21,84 @@
 
 async function createTransaction(req, res) {
 
+   /**
+    * 1. Validate request
+    */
    const {fromAccount, toAccount, amount, idempotenceKey } = req.body;
 
-}
+    if( !fromAccount || !toAccount || !amount || !idempotenceKey ) {
+          return res.status(400).json({ 
+            message: 'fromAccount , toAccount , amount , and idempotenceKey are required',
+            
+         })
+    }
+
+    const fromUserAccount = await accountModel.findOne({
+      _id: fromAccount,
+    })
+
+    const toUserAccount = await accountModel.findOne({
+      _id: toAccount,
+    })
+
+    if(!fromUserAccount || !toUserAccount) {
+      return res.status(404).json({
+        message: 'Invalid fromAccount or toAccount'
+      })
+    }
+
+
+    /**
+     * 2. Validate Idempotence Key
+     */
+
+    const isTransactionAlreadyExists = await transactinModel.findOne({
+      idempotenceKey: idempotenceKey
+    })
+
+    if(isTransactionAlreadyExists) {
+       if(isTransactionAlreadyExists.status === 'COMPLETED') {
+        return res.status(200).json({
+          message: 'Transaction already completed',
+          transaction: isTransactionAlreadyExists
+        })
+       } 
+
+       if(isTransactionAlreadyExists.status === 'PENDING') {
+        return res.status(200).json({
+          message: 'Transaction is still pending',
+        })
+       }
+
+       if(isTransactionAlreadyExists.status === 'FAILED') {
+         return res.status(200).json({
+            message: 'Transaction has failed',
+         })
+          }
+
+          if(isTransactionAlreadyExists.status === 'REVERSED') {
+            return res.status(200).json({
+              message: 'Transaction has been reversed',
+            })
+          }
+           
+    }
+
+
+    /**
+     * 3. Check account status
+     */
+
+    if (fromUserAccount.status !== 'ACTIVE' || toUserAccount.status !== 'ACTIVE') {
+      return res.status(400).json({
+        message: 'From or to account is not active'
+      })
+    }
+
+    /**
+     * 4. Derive Sender balance from ledger
+     */
+
+    
    
+}
--- src/models/account.model.js
+++ src/models/account.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const ledgerModel = require('./ledger.model');
 
 
 const accountSchema = new mongoose.Schema({
@@ -28,7 +29,15 @@ const accountSchema = new mongoose.Schema({
 
    accountSchema.index({ user: 1, status: 1 });
 
+ accountSchema.methods.getBalance = async function() {
+
+ }
+
+
 
     const accountModel = mongoose.model('account', accountSchema);
+
+
+
 
     module.exports = accountModel;
